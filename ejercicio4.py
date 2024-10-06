5e65d24119524d40e5d59e1407671ad3205a94ad
Ivan <ibolhoc1408@g.educaand.es>
2024-10-06 11:26:58 +0200

    Tercera version

--- ejercicio4.py
+++ ejercicio4.py
@@ -6,3 +6,5 @@ os.system('copy archivo.txt copia.txt')
 
 listar = os.listdir('backup\\') #Listando el directorio backup
 print(listar) # Mostrando por pantalla el listado del directorio.
+
+os.rename('copia.txt', 'movido.txt') # Script para mover archivo
